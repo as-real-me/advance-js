843660f39f0b418228d686819990a1909fbd699e
as-real-me <real.uiucse@gmail.com>
2020-06-15 06:50:27 +0600

    Truthy and falsy

---
+++ truthy-falsy.js
@@ -0,0 +1,22 @@
+//Falsy
+// 0
+// ""
+// Undefined
+// null
+// NaN
+
+//Truthy
+//All other elements including "0", " ", [], {}
+
+let name;
+console.log(name);
+if(name){
+    console.log("Condition is true");
+}else{
+    console.log("Condition is false");
+}
+
+
+
+// Learning here:
+// Clear understanding about true and false values
