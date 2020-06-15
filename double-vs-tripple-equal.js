ada38d0fd30434288489889feeb7c933c62bdf51
as-real-me <real.uiucse@gmail.com>
2020-06-15 16:45:46 +0600

    == vs ===

---
+++ double-vs-tripple-equal.js
@@ -0,0 +1,16 @@
+const first = 2;
+const second = "2";
+
+// comparing only value with double equal
+// if(first == second){
+//     console.log("Condition is true");
+// }else{
+//     console.log("Condition is false");
+// }
+
+// Same condition comparing with triple equal for both value and data type
+if(first === second){
+    console.log("Condition is true");
+}else{
+    console.log("Condition is false");
+}
